f01f208c0c57a9cd45e00d3e47cd47d223cbf274
jwdomingo <johnwill.domingo@gmail.com>
2016-02-03 20:43:53 -0800

    Create asynchronous worker server to make HTTP request on diffs between sites.txt and sites

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,44 @@
-// Use the code in `archive-helpers.js` to actually download the urls
-// that are waiting.
+#!/usr/local/bin/node
+
+var http = require('http');
+var fs = require('fs');
+
+var diff = [];
+
+fs.readFile(__dirname + '/../web/archives/sites.txt', 'utf-8', function(err, data) {
+  if (err) {
+    console.error(err);
+  } else {
+    var sites = data.split(',').forEach(function(site) {
+      fs.readdir(__dirname + '/../web/archives/sites', function(err, domains) {
+        if (err) {
+          console.error(err);
+        } else if (domains.indexOf(site) === -1) {
+          
+          var options = {
+            host: site,
+            port: 80,
+            path: '/index.html'
+          };
+
+          var buffer = '';
+          http.get(options, function(response){
+            response.on('data', function(chunk){
+              buffer += chunk.toString();
+            });
+
+            response.on('end', function() {
+              fs.writeFile(__dirname + '/../web/archives/sites/' + site, buffer, function(err) {
+                if (err) {
+                  console.error(err);
+                }
+              });
+            });
+          }).on('error', function(err){
+            console.error(err);
+          });
+        }
+      });
+    });    
+  }
+});
